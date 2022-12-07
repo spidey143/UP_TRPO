5b4633e56322d9876d448013c9082f68bb7166ca
@DanekBB777 <bekenev.daniilkpc582020@gmail.com>
2022-12-07 16:51:04 +0300

    Добавьте файлы проекта.

---
+++ Legkaya/ADD_SOTR.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Legkaya
+{
+    /// <summary>
+    /// Логика взаимодействия для ADD_SOTR.xaml
+    /// </summary>
+    public partial class ADD_SOTR : Window
+    {
+        public ADD_SOTR()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Legkaya/SOTr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legkaya
+{
+    class SOTr
+    {
+        public int Id { get; set; }
+        public string? personal_id { get; set; }
+        public string? last_name { get; set; }
+        public string? first_name { get; set; }
+        public string? patronymic { get; set; }
+        public string? date_of_birth { get; set; }
+        public string? phone { get; set; }
+        public string? otdel { get; set; }
+    }
+}
